af094f24ec728bdbf4d78d754f08a25f7f80a6d6
songzihengs.666 <songziheng.666@bytedance.com>
2022-12-09 09:57:29 +0800

    add redis test

--- src/build.sh
+++ src/build.sh
@@ -1,7 +1,13 @@
+#!/bin/zsh
+# cd /Users/bytedance/Desktop/my_proj/g55_chatroom/src
+# go mod init g55.com/chat
+export GO111MODULE=on
 export GOPATH="/Users/bytedance/go:/Users/bytedance/Desktop/my_proj/g55_chatroom"
 #go build -o main_run *.go
 go build -o server_run ./server_main.go
 go build -o client_run ./client_main.go
+go build -o db_run ./db_main.go
+
 if test $? -eq 0;
 then
     echo "compile done"
@@ -9,4 +15,4 @@ then
 else
     echo "compile error"
 fi
-chmod 777 server_run client_run
+chmod 777 server_run client_run db_run
